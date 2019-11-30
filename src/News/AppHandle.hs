c36e85548e7550110e2cd1085213bf2556495e98
Kirill Elizarov <kirillthetee@gmail.com>
2019-11-30 15:57:07 +0700

    completed implementing basic reader mtl for Applciation

--- src/News/AppHandle.hs
+++ src/News/AppHandle.hs
@@ -2,18 +2,16 @@
 
 module News.AppHandle where
 
-import qualified Control.Exception             as EX
-import           Control.Monad.IO.Class         ( MonadIO )
-import           Control.Monad.Reader           ( MonadReader
-                                                , ReaderT
-                                                , runReaderT
-                                                )
-import qualified Data.ByteString.Char8         as BS
-import qualified Database.PostgreSQL.Simple    as PSQL
-import qualified Network.Wai                   as WAI
-import           Network.Wai.Handler.Warp       ( run )
+import qualified Control.Exception as EX
+import Control.Monad.Fail (MonadFail)
+import Control.Monad.IO.Class (MonadIO)
+import Control.Monad.Reader (MonadReader, ReaderT, runReaderT)
+import qualified Data.ByteString.Char8 as BS
+import qualified Database.PostgreSQL.Simple as PSQL
+import qualified Network.Wai as WAI
+import Network.Wai.Handler.Warp (run)
 
-import           News.Config
+import News.Config
 
 data AppEnv = AppEnv
   { ahConfig :: AppConfig
@@ -23,10 +21,17 @@ data AppEnv = AppEnv
 
 newtype Application a = Application
   { runApp :: ReaderT AppEnv IO a
-  } deriving (Functor, Applicative, Monad, MonadIO, MonadReader AppEnv)
+  } deriving ( Functor
+             , Applicative
+             , Monad
+             , MonadFail
+             , MonadIO
+             , MonadReader AppEnv
+             )
 
-runApplication
-  :: AppConfig -> WAI.Request -> PSQL.Connection -> Application a -> IO a
+runApplication ::
+     AppConfig -> WAI.Request -> PSQL.Connection -> Application a -> IO a
 runApplication conf@AppConfig {..} request connection =
   (`runReaderT` env) . runApp
-  where env = AppEnv conf request connection
+  where
+    env = AppEnv conf request connection
--- src/News/Endpoints/User.hs
+++ src/News/Endpoints/User.hs
@@ -2,41 +2,39 @@
 
 module News.Endpoints.User where
 
-import qualified Control.Exception             as EX
-import           Control.Monad.IO.Class         ( MonadIO
-                                                , liftIO
-                                                )
-import           Control.Monad.Reader           ( asks )
-import qualified Core.Config                   as C
-import           Data.Aeson                     ( ToJSON(..) )
-import qualified Data.Aeson                    as J
-import qualified Data.Text                     as T
-import qualified Data.Time                     as Time
-import qualified Database.PostgreSQL.Simple    as PSQL
-import           GHC.Generics
-import           Network.Wai                    ( Request
-                                                , strictRequestBody
-                                                )
+import Control.Monad.Except
+import Control.Monad.IO.Class (MonadIO, liftIO)
+import Control.Monad.Reader (asks)
+import Data.Aeson (FromJSON(..), ToJSON(..), (.:))
+import GHC.Generics (Generic)
+import Network.Wai (Request, strictRequestBody)
 
-import           News.AppHandle
-import           News.Config
-import           News.Env
-import qualified News.Models.Persisted            as M
-import qualified News.Models.User              as M
-import           News.Services.Database.Config
-import           News.Services.Database.Queries.User
-import qualified News.UseCases.RegisterUser    as RegisterUser
+import qualified Control.Exception as EX
+import qualified Core.Config as C
+import qualified Data.Aeson as J
+import qualified Data.Text as T
+import qualified Data.Time as Time
+import qualified Database.PostgreSQL.Simple as PSQL
+
+import News.AppHandle
+import News.Config
+import News.Env
+
+import qualified News.Models.Persisted as M
+import qualified News.Models.User as M
+import qualified News.Services.Database.Queries.User as DB
+import qualified News.UseCases.RegisterUser as RegisterUser
 
 data Response a
   = Success a
   | InvalidRequest T.Text
-  | TransportError
+  | InternalError
   deriving (Show, Eq)
 
 instance (ToJSON a) => ToJSON (Response a) where
-  toJSON (Success        a  ) = J.toJSON a
+  toJSON (Success a) = J.toJSON a
   toJSON (InvalidRequest err) = J.object ["error" J..= err]
-  toJSON TransportError =
+  toJSON InternalError =
     J.object ["error" J..= ("Unexpected error happened!" :: String)]
 
 data UserSerialized = UserSerialized
@@ -46,25 +44,42 @@ data UserSerialized = UserSerialized
   , usAvatarPath :: T.Text
   , usCreatedAt :: Time.UTCTime
   , usIsAdmin :: Bool
-  } deriving (Show, Eq, Generic, ToJSON)
+  } deriving (Show, Eq, Generic)
 
-createUserHandler :: Application (Response UserSerialized)
-createUserHandler = do
-  req  <- asks ahRequest
-  conn <- asks ahConnection
-  body <- liftIO $ strictRequestBody req
-  liftIO $ print body
-  let firstname   = "John"
-      lastname    = "Doe"
-      persistUser = liftIO . createUser conn
-  res <- liftIO $ RegisterUser.execute
-    (RegisterUser.Handle persistUser putStrLn)
-    firstname
-    lastname
-  pure undefined
+serializePersistedUser :: M.Persisted M.User -> UserSerialized
+serializePersistedUser persistedUser =
+  let userId = M.getId persistedUser
+      M.User {..} = M.getObj persistedUser
+   in UserSerialized
+        { usId = M.fromID userId
+        , usFirstName = uFirstName
+        , usLastName = uLastName
+        , usAvatarPath = uAvatarPath
+        , usCreatedAt = uCreatedAt
+        , usIsAdmin = uIsAdmin
+        }
 
-listUsersHandler :: MonadIO m => Request -> m [UserSerialized]
-listUsersHandler = undefined
+data CreateUserRequest = CreateUserRequest
+  { curFirstName :: T.Text
+  , curLastName :: T.Text
+  } deriving (Show, Eq)
 
-getUserByIdHandler :: MonadIO m => Request -> m (Maybe UserSerialized)
-getUserByIdHandler = undefined
+instance FromJSON CreateUserRequest where
+  parseJSON =
+    J.withObject "CreateUserRequest" $ \o ->
+      CreateUserRequest <$> o .: "first_name" <*> o .: "last_name"
+
+-- TODO add exception handling of some sorts
+createUserEndpoint :: CreateUserRequest -> Application (Response UserSerialized)
+createUserEndpoint CreateUserRequest {..} = do
+  let hPersistUser = DB.createUser
+  let hLog = liftIO . putStrLn
+  persistedUser <-
+    RegisterUser.execute RegisterUser.Handle {..} curFirstName curLastName
+  pure . Success $ serializePersistedUser persistedUser
+
+listUsersEndpoint :: MonadIO m => Request -> m [UserSerialized]
+listUsersEndpoint = undefined
+
+getUserByIdEndpoint :: MonadIO m => Request -> m (Maybe UserSerialized)
+getUserByIdEndpoint = undefined
